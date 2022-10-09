4acd6dc5b1c3b82e7d72c913ea52d6ee7844e4df
fumia <tsuru@fumia.local>
2022-10-09 16:59:16 +0900

    fixed

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/accsess/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Box is Ownable{
     uint256 private _value;
 
     event ValueChanged(uint256 value);
 
-    function store(uint256 value) public onlyOwnwer{
-        _value = value:
+    function store(uint256 value) public onlyOwner{
+        _value = value;
         emit ValueChanged(value);
     }
 
